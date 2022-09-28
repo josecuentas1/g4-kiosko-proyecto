f66277782b74156b542e68b878235781a63ea05e
José Cuentas <72282358+restjcr@users.noreply.github.com>
2022-09-27 22:03:18 -0500

    conf. database

--- src/Modelo/Conexion.java
+++ src/Modelo/Conexion.java
@@ -10,11 +10,11 @@ public class Conexion {
 
     public Connection getConnection() {
         try {
-            String myBD = "jdbc:mysql://3.tcp.ngrok.io:23947/sis_java";
-            con = DriverManager.getConnection(myBD, "root", "123456a.");
+            //String myBD = "jdbc:mysql://3.tcp.ngrok.io:23947/sis_java";
+            //con = DriverManager.getConnection(myBD, "root", "123456a.");
             
-            //String myBD = "jdbc:mysql://localhost:3306/sis_java";
-            //con = DriverManager.getConnection(myBD, "root", "");
+            String myBD = "jdbc:mysql://localhost:3306/sis_java";
+            con = DriverManager.getConnection(myBD, "root", "1234");
             
             //String myBD = "jdbc:mysql://localhost:3306/gpsw";
             //con = DriverManager.getConnection("jdbc:mysql://sql10.freemysqlhosting.net/sql10484637","sql10484637","YrRRH2Lzpl");
